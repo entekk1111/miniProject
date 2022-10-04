3d1fdfa64f8ab323ffda70fb67ce8d419e9f1dda
사용자 <@DESKTOP-MIKL98R>
2022-10-04 13:01:44 +0900

    로그인 추가

--- miniProject/src/main/java/com/miniProject/common/security/handler/LoginSuccessHandler.java
+++ miniProject/src/main/java/com/miniProject/common/security/handler/LoginSuccessHandler.java
@@ -6,18 +6,36 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import com.miniProject.mapper.UserMapper;
+import com.miniProject.model.SessionVO;
+
 @Component
 public class LoginSuccessHandler implements AuthenticationSuccessHandler{
-
+	private static int TIME = 60 * 60; // 1시간
+	
+	@Autowired
+	UserMapper userMapper;
+	
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
-		System.out.println(authentication);
 		
+		request.getSession().setMaxInactiveInterval(TIME);
+        String username = (String)authentication.getPrincipal();
+//        System.out.println(authentication.getPrincipal());
+        
+        //유저객체 생성 
+        SessionVO user = userMapper.getUserInfo(username);
+        request.getSession().setAttribute("SID", user);
+//		System.out.println(authentication.getDetails());
+//		System.out.println(authentication.getPrincipal());
+//		CustomUserDetails customUserDetails = (CustomUserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//		System.out.println(customUserDetails.getSessionVO().getEmail());
 		response.sendRedirect("/product");
 	}
 
